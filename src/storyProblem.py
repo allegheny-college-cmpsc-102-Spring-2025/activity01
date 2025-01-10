7b178f57e7b0d5126c5a084968809f2005f5c703
hangzhaogogogo <hzhao@allegheny.edu>
2025-01-10 11:17:42 -0500

    init commit

---
+++ src/storyProblem.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Name: Add Your Name Here
+
+FIRST_VALUE = 0  # TODO: Replace 0 with the 1st value provided by Prof. Zhao
+SECOND_VALUE = 0  # TODO: Replace 0 with the 2nd value provided by Prof. Zhao
+
+print("Completed story problem:")
+print(f"What value do you get when you raise FIRST_VALUE to the power of SECOND_VALUE?")
+
+student_answer = 0  # TODO: Replace 0 with your answer
+
+correct_result = 0  # TODO: Replace 0 with a function
+
+if student_answer == correct_result:
+    print(f"The correct value is {correct_result}. You answered the problem correctly!")
+else:
+    print(f"The correct value is {correct_result}. Your answer is incorrect. Try again next time!")
